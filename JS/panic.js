d8166a43b0deaf57ec378f9cb68851b41b48a5df
Jaap Papavoine <jaap.papavoine@infosupport.com>
2018-11-23 11:17:05 +0100

    Fancy object instead of string

--- JS/panic.js
+++ JS/panic.js
@@ -1,10 +1,15 @@
 
 function panic(messagetype, trainlocation){
+    message = {}
+    message.trein = document.getElementById("trein").value,
+    message.tijd = Date()
+
     var bla = JSON.parse(window.localStorage.getItem('messages'));
     if(!bla) {
         bla = []
     }
-    bla.push("Nieuw bericht")
+    bla.push(message)
+    console.log("Nieuw bericht: Trein: " + message.trein + " Tijd: " + message.tijd)
     window.localStorage.setItem('messages', JSON.stringify(bla))
     alert(bla)
 }
